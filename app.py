81ccae537a19d92cc0c58cbbeb89618ba9596dfd
Kofi Dwomoh-Ababio <dakbill@yahoo.com>
2016-06-26 15:19:27 +0000

    adds get diagnose api

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ def home():
 
 @app.route('/api/diagnose')
 def get_likely_diagnose():
-    symptoms = request.args.get('symptoms')
-    print symptoms
-    return jsonify({'code': 0, 'message': 'Success', 'results': [{'name': 'Malaria'}]})
+    symptoms = request.args.get('symptoms').split(',')
+    likely_diagnoses = graph.run("MATCH (s:Symptom)-[r:IS_SYMPTOM_OF]->(d:Diagnose) WHERE %s return d.name as name" % ' or '.join(["s.description = '%s'" % str(s).lower() for s in symptoms]))
+    return jsonify({'code': 0, 'message': 'Success', 'results': [dict(likely_diagnose) for likely_diagnose in likely_diagnoses]})
 
 @app.route('/api/diagnose', methods=['POST'])
 def create_diagnose():
     data = request.get_json()
-    graph.run("CREATE (d:Diagnose {name:'%s'}), %s " % (data.get('diagnose'), ','.join(["(s%d:Symptom{description:'%s'}), s%d-[r%d:IS_SYMPTOM_OF]->d" % (i, s, i, i) for i, s in enumerate(data.get('symptoms'), 1)])))
+    graph.run("CREATE (d:Diagnose {name:'%s'}), %s " % (data.get('diagnose'), ','.join(["(s%d:Symptom{description:'%s'}), s%d-[r%d:IS_SYMPTOM_OF]->d" % (i, str(s).lower(), i, i) for i, s in enumerate(data.get('symptoms'), 1)])))
     return jsonify({'code': 0, 'message': 'Success', 'results': {}})
 
 
